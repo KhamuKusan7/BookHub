5d4e830c309c44710d7ca366da279e4c12d6e747
KhamuKusan7 <83013033+KhamuKusan7@users.noreply.github.com>
2025-10-12 17:23:20 +0545

    Wrap App with div and main layout including Navbar and Footer in index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './MyComponents/Navbar.jsx';
-import Footer from './MyComponents/Footer.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./MyComponents/Navbar.jsx";
+import Footer from "./MyComponents/Footer.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="*" element={<App />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   </React.StrictMode>
 );
