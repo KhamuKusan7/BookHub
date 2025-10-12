e6c6ddf7e7c92b83dd36a3507e088d0d49a2163b
KhamuKusan7 <83013033+KhamuKusan7@users.noreply.github.com>
2025-10-12 16:28:04 +0545

    Add HarryPotter1 route and component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from './MyComponents/Home';
 import BookProfile from './MyComponents/BookProfile'
 import './App.css'
 import { Routes, Route} from 'react-router-dom';
+import HarryPotter1 from './MyComponents/HarryPotter1';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/BookProfile" element={<BookProfile />}/>
+        <Route path="/HarryPotter1" element={<HarryPotter1 />} />
         {/* <Route path="" element={<Profile />}/> */}
         {/* <Route path="" element={<Toggle />}/> */}
         {/* <Route path="" element={<Logout />}/> */}
