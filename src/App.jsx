b9f96a7da630ebb2cb7be4321c22b23cb8b1f330
KhamuKusan7 <83013033+KhamuKusan7@users.noreply.github.com>
2025-10-14 22:30:51 +0545

    add Route for Profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import BookDetail from "./MyComponents/BookDetail";
-import BookProfile from "./MyComponents/BookProfile";
 import Home from "./MyComponents/Home";
+import BookProfile from "./MyComponents/BookProfile";
+import BookDetail from "./MyComponents/BookDetail";
+import Profile from "./MyComponents/Profile";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/BookProfile" element={<BookProfile />} />
       <Route path="/book/:id" element={<BookDetail />} />
+      <Route path="/Profile" element={<Profile />} />
     </Routes>
   );
 }
